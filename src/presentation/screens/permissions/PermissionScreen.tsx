2e61acfb9ad5b988d46ca464dd04b8da94dd0ed3
Manuel Gomez <jmgc@virket.com>
2024-07-03 09:04:17 -0600

    first commit

---
+++ src/presentation/screens/permissions/PermissionScreen.tsx
@@ -0,0 +1,12 @@
+
+import { Text, View } from 'react-native'
+
+export const PermissionScreen = () => {
+    return (
+        <View>
+            <Text>
+                PermissionScreen
+            </Text>
+        </View>
+    )
+}
