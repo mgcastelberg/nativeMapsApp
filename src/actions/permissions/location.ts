3bdb88ce7b92a1753ef811a77abaac6601dc8675
Manuel Gomez <jmgc@virket.com>
2024-07-09 08:47:08 -0600

    Revisar permiso de ubicación

--- src/actions/permissions/location.ts
+++ src/actions/permissions/location.ts
@@ -1,11 +1,11 @@
-import { openSettings, PERMISSIONS, request, PermissionStatus as RNPermissionsStatus } from "react-native-permissions";
+import { check, openSettings, PERMISSIONS, request, PermissionStatus as RNPermissionsStatus } from "react-native-permissions";
 import type { PermissionStatus } from "../../infraestructure/interfaces/permissions";
 import { Platform } from "react-native";
 
 
 export const requestLocationPermission = async ():Promise<PermissionStatus> => {
     let status: RNPermissionsStatus = 'unavailable';
-
+    // request abre un cuadro de dialogo
     if ( Platform.OS === 'ios' ) {
         status = await request( PERMISSIONS.IOS.LOCATION_WHEN_IN_USE )
     } else if( Platform.OS === 'android' ) {
@@ -31,4 +31,29 @@ export const requestLocationPermission = async ():Promise<PermissionStatus> => {
 
     return permissionMapper[status] ?? "undetermined";
 
+}
+
+export const checkLocationPermission = async ():Promise<PermissionStatus> => {
+
+    let status: RNPermissionsStatus = 'unavailable';
+    // check solo nos dice si tenemos permisos
+    if ( Platform.OS === 'ios' ) {
+        status = await check( PERMISSIONS.IOS.LOCATION_WHEN_IN_USE )
+    } else if( Platform.OS === 'android' ) {
+        status = await check( PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION )
+    } else {
+        status = 'unavailable';
+        throw new Error("Platform not supported");
+    }
+
+    // Mapa como si usaramos un switch
+    const permissionMapper: Record<RNPermissionsStatus, PermissionStatus> = {
+        'granted': 'granted',
+        'denied': 'denied',
+        'blocked': 'blocked',
+        'unavailable': 'unavailable',
+        'limited': 'limited'
+    };
+
+    return permissionMapper[status] ?? "undetermined";
 }
